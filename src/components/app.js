e5c0f305c46e7b704ad9fd2b09db2e9a09112815
tae-lim <taelim92@gmail.com>
2018-08-17 15:56:45 -0700

    App renders

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,21 @@
 angular.module('video-player')
+.controller('videoController', function () {
+  this.videoList = window.exampleVideoData;
+})
 
-.component('app', {
-  // TODO
-});
+.component('videoBody', {
+  controller: 'videoController',
+  templateUrl: 'src/templates/app.html'
+})
+
+
+
+
+// angular.module('video-player')
+//  controller: function () {
+//     this.videoList = window.exampleVideoData;
+//  }
+// .component('app', {
+//   templateUrl: 'src/templates/app.html'
+// });
+
